8418a236814355eb782b9f04ee110ae043d1312c
Jonathan Argueta-Herrera <jonathanargueta007@gmail.com>
2023-12-08 22:32:15 -0600

    1st commit

---
+++ src/main/java/dmacc/SpringSoccer2Application.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoccer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoccer2Application.class, args);
+	}
+
+}
---
+++ src/main/java/dmacc/controller/WebController.java
@@ -0,0 +1,62 @@
+package dmacc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+import dmacc.beans.Player;
+import dmacc.repository.PlayerRepository;
+
+@Controller
+public class WebController {
+	@Autowired
+	PlayerRepository repo;
+
+	@GetMapping({"viewAll"})
+	public String viewAllPlayers(Model model) {
+		if (repo.findAll().isEmpty()) {
+			return addNewPlayer(model);
+		}
+		model.addAttribute("players", repo.findAll());
+		return "results";
+	}
+
+	@GetMapping("/inputPlayer")
+	public String addNewPlayer(Model model) {
+		Player p = new Player();
+		model.addAttribute("newPlayer", p);
+		return "input";
+	}
+
+	@PostMapping("/inputPlayer")
+	public String addNewPlayer(@ModelAttribute Player p, Model model) {
+		repo.save(p);
+		return viewAllPlayers(model);
+	}
+
+	@GetMapping("/edit/{id}")
+	public String showUpdatePlayer(@PathVariable("id") long id, Model model) {
+		Player p = repo.findById(id).orElse(null);
+		System.out.println("ITEM TO EDIT: " + p.toString());
+		model.addAttribute("newPlayer", p);
+		return "input";
+	}
+
+	@PostMapping("/update/{id}")
+	public String revisePlayer(Player p, Model model) {
+		repo.save(p);
+		return viewAllPlayers(model);
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model) {
+		Player p = repo.findById(id).orElse(null);
+		repo.delete(p);
+		return viewAllPlayers(model);
+	}
+
+}
